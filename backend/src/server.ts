ec1a0f295367f9fb50d02d29d192517fa8dea885
Sai Han Htet <sai.hanhtetsan@gmail.com>
2026-03-21 16:04:53 +0700

    package.json updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
-//
+import path from 'path';
 import authRoute from './routes/auth.route.ts';
 import messageRoute from './routes/message.route.ts';
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const port = process.env.PORT || 3001;
+const env = process.env.NODE_ENV || 'development';
 
 app.use('/api/auth', authRoute);
 app.use('/api/messages', messageRoute);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+if (env === 'production') {
+    const static_assets = express.static(path.resolve(__dirname, '../frontend/dist'));
+    app.use(static_assets);
+
+    app.get('/*splat', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
